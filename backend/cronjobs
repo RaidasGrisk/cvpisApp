1550d3fcb2c2a551ad6471bcadb6396aae54b038
RaidasGrisk <mrraidas@gmail.com>
2020-12-18 16:43:34 +0200

    fix typos

---
+++ backend/cronjobs
@@ -0,0 +1,2 @@
+*/2 * * * * python3 /home/mrraidas/cvpisApp/backend/update_db.py >/dev/null 2>&1
+*/1 * * * * python3 /home/mrraidas/cvpisApp/backend/test_job.py > /home/mrraidas/cvpisApp/backend/logs/test_job.log
--- backend/test_job.py
+++ backend/test_job.py
@@ -1,2 +1,3 @@
-*/2 * * * * python3 /home/mrraidas/cvpisApp/backend/update_db.py >/dev/null 2>&1
-*/1 * * * * python3 /home/mrraidas/cvpisApp/backend/test_job.py > /home/mrraidas/cvpisApp/backend/logs/test_job.log
+
+if __name__ == '__main__':
+    print('done!')
